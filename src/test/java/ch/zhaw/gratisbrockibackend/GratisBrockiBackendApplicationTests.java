d7709313e8b06c47cc89c3b176e7fd6b4b1c3c2f
Alexander Huber <alexander.huber@bedag.ch>
2022-05-30 15:18:18 +0200

    initial commit

---
+++ src/test/java/ch/zhaw/gratisbrockibackend/GratisBrockiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.gratisbrockibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GratisBrockiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
