be8c580cae99ff7ed6c16e3cf182e53a81798fc7
Andreas Blaser <andreas.blaser@outlook.com>
2022-06-19 00:33:11 +0200

    remove redundant methods and add mappings to ignore some attributes

--- src/main/java/ch/zhaw/gratisbrockibackend/mapper/UserMapper.java
+++ src/main/java/ch/zhaw/gratisbrockibackend/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package ch.zhaw.gratisbrockibackend.mapper;
 import ch.zhaw.gratisbrockibackend.domain.User;
 import ch.zhaw.gratisbrockibackend.dto.UserCreationDto;
 import ch.zhaw.gratisbrockibackend.dto.UserDto;
-import ch.zhaw.gratisbrockibackend.dto.UserUpdateDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -11,12 +10,18 @@ import org.mapstruct.Mapping;
 public interface UserMapper {
 
     UserDto toUserDto (User user);
-    UserCreationDto toUserCreationDto(User user);
-    UserUpdateDto toUserUpdateDto(User user);
 
-    User toUser (UserDto userDto);
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "lastModifiedDate", ignore = true)
+    @Mapping(target = "items", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "enabled", ignore = true)
+    @Mapping(target = "credentialsNonExpired", ignore = true)
+    @Mapping(target = "createdDate", ignore = true)
+    @Mapping(target = "archivedDate", ignore = true)
+    @Mapping(target = "accountNonLocked", ignore = true)
+    @Mapping(target = "accountNonExpired", ignore = true)
     User toUser (UserCreationDto userCreationDto);
-    User toUser (UserUpdateDto userUpdateDto);
 
 }
 
