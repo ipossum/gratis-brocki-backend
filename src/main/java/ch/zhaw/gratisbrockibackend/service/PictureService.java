1fb0d80a5e74505e3fc57d524d0e943717dcc046
Andreas Blaser <andreas.blaser@outlook.com>
2022-06-21 00:08:37 +0200

    introduced PictureMapper and PictureCreationDto; made adjustments to PictureController and PictureService accordingly; prepared PictureValidator

--- src/main/java/ch/zhaw/gratisbrockibackend/service/PictureService.java
+++ src/main/java/ch/zhaw/gratisbrockibackend/service/PictureService.java
@@ -1,9 +1,14 @@
 package ch.zhaw.gratisbrockibackend.service;
 
 import ch.zhaw.gratisbrockibackend.domain.Picture;
+import ch.zhaw.gratisbrockibackend.dto.PictureCreationDto;
+import ch.zhaw.gratisbrockibackend.dto.PictureDto;
+import ch.zhaw.gratisbrockibackend.mapper.PictureMapper;
 import ch.zhaw.gratisbrockibackend.repository.PictureRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
 
@@ -12,8 +17,32 @@ import java.util.List;
 public class PictureService {
     private final PictureRepository pictureRepository;
 
-    public List<Picture> getPictures() {
-        return pictureRepository.findAll();
+    private final PictureMapper pictureMapper;
+
+    public List<PictureDto> getPictures() {
+        return pictureRepository.findAll()
+                .stream()
+                .map(pictureMapper::toPictureDto)
+                .toList();
     }
 
+    public ResponseEntity<PictureDto> addNewPicture(PictureCreationDto pictureCreationDto) {
+        try {
+            // TODO: add some validation logic with pictureValidator
+            Picture picture = pictureMapper.toPicture(pictureCreationDto);
+            pictureRepository.save(picture);
+            return ResponseEntity.ok(pictureMapper.toPictureDto(picture));
+        } catch (HttpClientErrorException.BadRequest e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    public void deletePicture(Long id) {
+        try {
+            pictureRepository.deleteById(id);
+        } catch (HttpClientErrorException.BadRequest e) {
+            e.printStackTrace();
+        }
+    }
 }
