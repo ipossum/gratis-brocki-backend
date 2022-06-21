d1207f2eb629cceb834e0528336aaf93eb5b525d
Alexander Huber <alexander.huber@bedag.ch>
2022-06-21 16:30:21 +0200

    CORS enabled

--- src/main/java/ch/zhaw/gratisbrockibackend/controller/UserController.java
+++ src/main/java/ch/zhaw/gratisbrockibackend/controller/UserController.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RequestMapping("api/v1/users")
 @RestController
 public class UserController {
