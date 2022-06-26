d5420390829422729df7c7ea7669d3f534e1d395
Andreas Blaser <andreas.blaser@outlook.com>
2022-06-26 23:37:46 +0200

    minor adjustments and remove redundancy

--- src/main/java/ch/zhaw/gratisbrockibackend/GratisBrockiBackendApplication.java
+++ src/main/java/ch/zhaw/gratisbrockibackend/GratisBrockiBackendApplication.java
@@ -23,7 +23,6 @@ public class GratisBrockiBackendApplication implements HasLogger {
     @PostConstruct
     public void afterInit() {
         boolean hasDevProfile = Arrays.asList(env.getActiveProfiles()).contains("dev");
-        getLogger().info("Active Profiles: " + Arrays.toString(env.getActiveProfiles()) + "\n\n");
         boolean hasH2Database = Arrays.asList(env.getActiveProfiles()).contains("h2");
         String applicationName = env.getProperty("spring.application.name");
         String openApiInfo="";
@@ -31,8 +30,7 @@ public class GratisBrockiBackendApplication implements HasLogger {
         if (hasDevProfile) {
             openApiInfo = """
          http://localhost:8080/v3/api-docs
-         http://localhost:8080/v3/api-docs.yaml -> yaml file is downloaded -> 
-        https://editor.swagger.io/
+         http://localhost:8080/v3/api-docs.yaml -> yaml file is downloaded -> https://editor.swagger.io/
          http://localhost:8080/swagger-ui.html\s
          """;
         }
@@ -40,9 +38,9 @@ public class GratisBrockiBackendApplication implements HasLogger {
             h2ConsoleInfo= "http://localhost:8080/h2-console " + "" +
                     "-> mit Generic H2 (Embedded), org.h2.Driver, jdbc:h2:mem:testdb und sa \n";
         }
-        System.out.println("\n\nApplication [" + applicationName + "] - Enter in Browser:\nhttp://localhost:8080 \n" +
-        openApiInfo +
+        System.out.println("\nApplication [" + applicationName + "] - Enter in Browser:\nhttp://localhost:8080 \n" +
+                openApiInfo +
                 h2ConsoleInfo + "\n" +
-                "Active Profiles: " + Arrays.toString(env.getActiveProfiles()) + "\n\n");
+                "Active Profiles: " + Arrays.toString(env.getActiveProfiles()) + "\n");
     }
 }
