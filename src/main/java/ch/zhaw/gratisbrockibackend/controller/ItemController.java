69a30085c1ccff38078b52f09f7cc0a206afca28
Andreas Blaser <andreas.blaser@outlook.com>
2022-06-26 23:57:05 +0200

    added missing bracket

--- src/main/java/ch/zhaw/gratisbrockibackend/controller/ItemController.java
+++ src/main/java/ch/zhaw/gratisbrockibackend/controller/ItemController.java
@@ -43,6 +43,7 @@ public class ItemController {
                 .stream()
                 .map(itemMapper::toItemDto)
                 .toList();
+    }
 
     @GetMapping("/{id}")
     public ItemDto getItem(@PathVariable("id") Long id) {
