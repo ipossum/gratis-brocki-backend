2677b6cd2e73a4303bfda82f00830f9b61dc7202
Andreas Blaser <andreas.blaser@outlook.com>
2022-07-07 16:34:58 +0200

    removed unnecessary imports

--- src/main/java/ch/zhaw/gratisbrockibackend/dto/FileDto.java
+++ src/main/java/ch/zhaw/gratisbrockibackend/dto/FileDto.java
@@ -1,16 +1,7 @@
 package ch.zhaw.gratisbrockibackend.dto;
 
-import ch.zhaw.gratisbrockibackend.domain.BaseEntity;
-import ch.zhaw.gratisbrockibackend.domain.Item;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
 
 @Getter
 @Setter
